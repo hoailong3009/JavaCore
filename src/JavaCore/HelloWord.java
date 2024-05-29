97030dd534afec6d107ecbf1914bd6f99f0c6f91
hoailong3009 <90884779+hoailong3009@users.noreply.github.com>
2024-05-29 10:43:06 +0700

    init project

---
+++ src/JavaCore/HelloWord.java
@@ -0,0 +1,9 @@
+package JavaCore;
+
+public class HelloWord {
+
+	public static void main(String[] args) {
+		System.out.println("Hello word with Long");
+
+	}
+}
---
+++ src/module-info.java
@@ -0,0 +1,9 @@
+/**
+ * 
+ */
+/**
+ * @author hoailong
+ *
+ */
+module JavaCore {
+}
